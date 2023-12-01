9f1f27e8e83aa246274b6898c049c0b1a4d1e36f
Michal Myška <47862818+mysakm@users.noreply.github.com>
2023-12-01 23:00:20 +0100

    Popup pro "About"
    s nicky, předělejte si to na jména, můžete si získat kredit z "práce na popupu 'About'" :3

--- demo3/src/main/java/com/example/demo3/HelloController.java
+++ demo3/src/main/java/com/example/demo3/HelloController.java
@@ -6,6 +6,12 @@ import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -27,6 +33,8 @@ public class HelloController {
     protected void aboutPopup() {
         Label label = new Label("Paint app v1.0\nby: Grumbajzik, Jurajs_, Alton, ThatMeow\nFor PEPE, with love <3");
         Button button = new Button("Close");
+        Image eastereggimg = new Image("http://media.tenor.com/y8R_Sz09brMAAAAi/faruzan-genshin-impact.gif");
+        ImageView easteregg = new ImageView(eastereggimg);
         TilePane tilepane = new TilePane();
         EventHandler<ActionEvent> closeIt = new EventHandler<ActionEvent>(){
             public void handle(ActionEvent actionEvent) {
@@ -36,9 +44,21 @@ public class HelloController {
         button.setOnAction(closeIt);
         tilepane.getChildren().add(label);
         tilepane.getChildren().add(button);
+        tilepane.getChildren().add(easteregg);
+        easteregg.setVisible(false);
         tilepane.setMaxWidth(300);
         tilepane.setBackground(new Background(new BackgroundFill(new Color(1, 1, 1, 1), new CornerRadii(10), new Insets(1))));
         popup.getContent().add(tilepane);
+        boolean[] boolList = {false, false, false, false};
+        final KeyCombination keycombo =  new KeyCodeCombination(KeyCode.DOWN, KeyCombination.CONTROL_DOWN);
+        popup.getScene().setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                    if(keycombo.match(event)){
+                        easteregg.setVisible(true);
+                    }
+                }
+        });
         popup.show(InsertImgF.getScene().getWindow());
     }
     protected void exitPopup(){
