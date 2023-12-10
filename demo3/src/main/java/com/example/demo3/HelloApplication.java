881becaefe6616bf860e1583ccaa7d3cd3391955
Altongun <125462416+Altongun@users.noreply.github.com>
2023-12-10 11:23:39 +0100

    Minimální size celé scény

--- demo3/src/main/java/com/example/demo3/HelloApplication.java
+++ demo3/src/main/java/com/example/demo3/HelloApplication.java
@@ -11,7 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setMinHeight(700);
+        stage.setMinWidth(700);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
         stage.setTitle("JavaFX!");
         stage.setScene(scene);
         stage.show();
