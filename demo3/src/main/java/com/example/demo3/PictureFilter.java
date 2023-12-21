4f6076defa2f733641421ea47df8173efd9fffd8
Jurajs_ <124284188+MarekMarmu@users.noreply.github.com>
2023-12-21 19:02:06 +0100

    Michal Myška

--- demo3/src/main/java/com/example/demo3/PictureFilter.java
+++ demo3/src/main/java/com/example/demo3/PictureFilter.java
@@ -3,8 +3,8 @@ package com.example.demo3;
 public class PictureFilter {
     //TODO: generování obrázku - Michal Myška
 
-    //TODO: Filtry:
-    // negativ - Grumby,
-    // RGB - Soldy,
+    //TODO: Filtry: Michal Myška
+    // negativ - Michal Myška,
+    // RGB - Michal Myška,
     // černobílá - Michal Myška
 }
