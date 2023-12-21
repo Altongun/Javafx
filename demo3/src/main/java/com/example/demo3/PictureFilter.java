df77d0f67220600e2ecd5b37b6ab8413cde81ddb
Grumbajzik <bobat@email.cz>
2023-12-21 18:55:56 +0100

    Zadání práce a tímto přeji Veselé Vánoce a méně smradu od soldánů

---
+++ demo3/src/main/java/com/example/demo3/PictureFilter.java
@@ -0,0 +1,10 @@
+package com.example.demo3;
+
+public class PictureFilter {
+    //TODO: generování obrázku - Michal Myška
+
+    //TODO: Filtry:
+    // negativ - Grumby,
+    // RGB - Soldy,
+    // černobílá - Michal Myška
+}
