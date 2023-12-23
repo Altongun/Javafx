a5345cdd8802a7754d102cc89dc3582c99edf4a0
ThatMeow <47862818+mysakm@users.noreply.github.com>
2023-12-23 13:30:53 +0100

    Upraven about popup

--- demo3/src/main/java/com/example/demo3/HelloController.java
+++ demo3/src/main/java/com/example/demo3/HelloController.java
@@ -1,27 +1,16 @@
 package com.example.demo3;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.TilePane;
-import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
-import javafx.stage.Popup;
+import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -45,7 +34,6 @@ public class HelloController {
     private MenuItem filterVin;
     @FXML
     private MenuItem filterCol;
-    Popup popup = new Popup();
 
     @FXML
     private Button InsertImgF;
@@ -64,9 +52,19 @@ public class HelloController {
     protected void exitProgram() {
         System.exit(0);
     }
-
-    //TODO: chtělo by udělat pravděpodobně nový fxml s tím co má být v about, aby se to otevřelo jako nové okno a né widget - Hnízdil
     @FXML
+    protected void aboutPopup() {
+        FXMLLoader aboutScreenFXML = new FXMLLoader(HelloApplication.class.getResource("about-view.fxml"));
+        Stage popupStage = new Stage();
+        popupStage.setTitle("About");
+        try{
+            popupStage.setScene(new Scene(aboutScreenFXML.load(), 300, 150));
+        }catch (Exception ignored){
+
+        }
+        popupStage.show();
+    }
+    /*@FXML
     protected void aboutPopup() {
         Label label = new Label("Paint app v1.0\nby: Grumbajzik, Jurajs_, Alton, ThatMeow\nFor PEPE, with love <3");
         Button button = new Button("Close");
@@ -96,7 +94,7 @@ public class HelloController {
                 }
         });
         popup.show(InsertImgF.getScene().getWindow());
-    }
+    }*/
     @FXML
     protected void loadPicture(){
         FileChooser filechooser = new FileChooser();
@@ -125,9 +123,7 @@ public class HelloController {
         mainimage.setPreserveRatio(true);
         mainimage.setFitHeight(InsertImgF.getScene().getHeight()/2.0f);
         mainimage.setFitWidth(InsertImgF.getScene().getWidth()/2.0f); //první setup výšky a šířky
-        InsertImgF.getScene().heightProperty().addListener((obs, oldVal, newVal) -> {
-            mainimage.setFitHeight(newVal.floatValue()/2.0f);
-        });
+        InsertImgF.getScene().heightProperty().addListener((obs, oldVal, newVal) -> mainimage.setFitHeight(newVal.floatValue()/2.0f));
         InsertImgF.getScene().widthProperty().addListener((obs, oldVal, newVal) -> {
             mainimage.setFitWidth(newVal.floatValue()/2.0f); // odposluchače na změnu výšky a šířky, upraví výšku/šířku obrázku
         });
